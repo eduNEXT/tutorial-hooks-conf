11f1b862a5bd0e94526e2a0850f39262c4c5123e
Felipe Montoya <felipe.montoya@edunext.co>
2023-03-24 12:20:25 -0500

    feat: testing the enrollment receiver

--- tests/test_receivers.py
+++ tests/test_receivers.py
@@ -2,10 +2,51 @@
 """
 Tests for the `tutorial-hooks-conf` receivers module.
 """
+import datetime
+
+from django.test import TestCase
+from opaque_keys.edx.keys import CourseKey
+from openedx_events.learning.data import CourseData, CourseEnrollmentData, UserData, UserPersonalData
+from openedx_events.learning.signals import COURSE_ENROLLMENT_CREATED
+
+from tutorial_hooks_conf.receivers import send_enrollment_data_to_webhook
 
 
-def test_placeholder():
+class EnrollmentReceiverTest(TestCase):
     """
-    Placeholder to allow pytest to succeed before real tests are in place.
-    (If there are no tests, it will exit with code 5.)
+    Tests the registration receiver processes the data correctly.
     """
+
+    def setUp(self):
+        """
+        Setup common conditions for every test case.
+        """
+        super().setUp()
+        self.enrollment = CourseEnrollmentData(
+            user=UserData(
+                pii=UserPersonalData(
+                    username="student",
+                    email="student@myuniversity.com",
+                    name="A Full Name",
+                ),
+                id=42,
+                is_active=True,
+            ),
+            course=CourseData(
+                course_key=CourseKey.from_string("course-v1:Demo+HooksTutorial+2023"),
+                display_name="Hooks Tutorial Course",
+            ),
+            mode="honor",
+            is_active=True,
+            creation_date=datetime.datetime(2023, 3, 28, 15, 55, 00),
+        )
+
+    def test_receiver_called(self):
+        """
+        Test that receiver is called with the correct information.
+        """
+        COURSE_ENROLLMENT_CREATED.connect(send_enrollment_data_to_webhook)
+
+        COURSE_ENROLLMENT_CREATED.send_event(
+            enrollment=self.enrollment,
+        )
